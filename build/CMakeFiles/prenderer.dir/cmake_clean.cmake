e8344e7593728ae6a11e1eba91d830c3a30609e2
pizhd <pizd@qq.com>
2022-04-05 23:44:57 +0800

    创建build

---
+++ build/CMakeFiles/prenderer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prenderer.dir/src/main/main_developer.cpp.o"
+  "CMakeFiles/prenderer.dir/src/models/model/test.cpp.o"
+  "prenderer"
+  "prenderer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prenderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
